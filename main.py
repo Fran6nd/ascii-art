d0efa5953216a30db352f8fa6819bfb04d78c643
Fran6nd <francoisnd1@gmail.com>
2021-09-29 12:51:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from PIL import Image
-im = Image.open('index.jpg')
+import sys
+im = Image.open(sys.argv[1])
 
 pixels = im.load()
 width, height = im.size
-print(width, height)
-output = ""
-chars = ["  ", "--", "::", "||", "[]", "00", "&&", "##"]
+
 for x in range(0, width):
     for y in range(0, height):
         #print(pixels[x,y])
@@ -14,11 +13,28 @@ for x in range(0, width):
         b = pixels[x,y][2]
         avg = int( (r +g + b) / 3)
         pixels[x,y] = (avg,avg,avg)
+
+im.thumbnail((100,100),Image.ANTIALIAS)
+im.save("test.jpg")
+
+
+im = Image.open('test.jpg')
+pixels = im.load()
+width, height = im.size
+print(width, height)
+output = ""
+chars = ["  ", "--", "::", "||", "[]", "OO", "00", "&&", "##"]
+
+
+for x in range(0, width):
+    for y in range(0, height):
+        #print(pixels[x,y])
+        avg = pixels[x,y][0]
         avg = (int) (avg / 256 * len(chars))
         output = output + chars[avg]
         
 
     output = output + "\n"
-im.save("test.png")
-with open("output.txt", "w") as f:
-    f.write(output)
+#with open("output.txt", "w") as f:
+#    f.write(output)
+print(output)
