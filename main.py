d0e7d061376066288fec6e926bae51e0caaefdd9
Your Name <you@example.com>
2021-11-05 19:56:35 +0100

    wip

--- main.py
+++ main.py
@@ -1,51 +1,61 @@
 #!/usr/local/bin/python3
 from PIL import Image, ImageFont, ImageDraw
 import sys
-im = Image.open(sys.argv[1])
+def process_pil_img(im):
+    pixels = im.load()
+    width, height = im.size
 
-pixels = im.load()
-width, height = im.size
-
-for x in range(0, width):
-    for y in range(0, height):
-        #print(pixels[x,y])
-        r = pixels[x,y][0]
-        g = pixels[x,y][1]
-        b = pixels[x,y][2]
-        avg = int( (r +g + b) / 3)
-        pixels[x,y] = (avg,avg,avg, 255)
-
-im.thumbnail((150,150),Image.ANTIALIAS)
-#im.save("test.png")
-
-
-#im = Image.open('test.png')
-pixels = im.load()
-width, height = im.size
-
-output = ""
-chars = ["  ", "::", "--", "||", "VA", "OO", "00", "&&", "##"]
-
-
-
-for y in range(0, height):
     for x in range(0, width):
-        #print(pixels[x,y])
-        avg = pixels[x,y][0]
-        avg = (int) (avg / 256 * len(chars))
-        output = output + chars[avg]
+        for y in range(0, height):
+            #print(pixels[x,y])
+            r = pixels[x,y][0]
+            g = pixels[x,y][1]
+            b = pixels[x,y][2]
+            avg = int( (r +g + b) / 3)
+            pixels[x,y] = (avg,avg,avg, 255)
+
+    #im.thumbnail((150,150),Image.ANTIALIAS)
+    #im.save("test.png")
+
+
+    #im = Image.open('test.png')
+    pixels = im.load()
+    width, height = im.size
+
+    output = ""
+    chars = ["  ", "::", "--", "||", "VA", "OO", "00", "&&", "##"]
+
+
+    for y in range(0, height):
+        for x in range(0, width):
+            #print(pixels[x,y])
+            avg = pixels[x,y][0]
+            avg = (int) (avg / 256 * len(chars))
+            output = output + chars[avg]
+        output = output + "\n"
+    return output
+def text_to_img(text, size, color = (0,0,0)):
+        img = Image.new('RGB', (size[0] * 15, size[1]*15), color = (0, 0,0))
         
+        fnt = ImageFont.truetype('./terminus.ttf', 15)
+        d = ImageDraw.Draw(img)
+        d.text((0,0), text, font=fnt, fill=color)
+        return img
+def main():
+    im = Image.open(sys.argv[1])
 
-    output = output + "\n"
-if len(sys.argv) == 2: 
-    #with open("output.txt", "w") as f:
-       # f.write(output)
-    print(output)
 
-else:
-    img = Image.new('RGB', (width * 15, height*15), color = (0, 0,0))
-    
-    fnt = ImageFont.truetype('./terminus.ttf', 15)
-    d = ImageDraw.Draw(img)
-    d.text((0,0), output, font=fnt, fill=(255, 255, 255))
-    img.save(sys.argv[2])
+
+            
+
+    output = process_pil_img(im)
+    if len(sys.argv) == 2: 
+        #with open("output.txt", "w") as f:
+        # f.write(output)
+        print(output)
+
+    else:
+        im.save(text_to_img(output, im.size))
+
+if __name__ == '__main__':
+    main()
---
+++ main1.py
@@ -0,0 +1,32 @@
+"""
+Simply display the contents of the webcam with optional mirroring using OpenCV 
+via the new Pythonic cv2 interface.  Press <esc> to quit.
+"""
+import cv2
+import numpy as np
+from PIL import Image
+import main
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+            color_coverted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            pil_image=Image.fromarray(color_coverted)
+
+            pil_image.thumbnail((int(pil_image.size[0]/15/1.5), int(pil_image.size[1]/15/1.5)), Image.ANTIALIAS)
+            pil_image = main.text_to_img(main.process_pil_img(pil_image), pil_image.size, (0,255,0))
+            numpy_image=np.array(pil_image)  
+
+            # convert to a openCV2 image, notice the COLOR_RGB2BGR which means that 
+            # the color is converted from RGB to BGR format
+            img=cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR) 
+            cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+        cv2.destroyAllWindows()
+def if_main():
+    show_webcam(mirror=True)
+if __name__ == '__main__':
+    if_main()
